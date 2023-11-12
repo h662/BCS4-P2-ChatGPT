04db4d8d858c8e83809ae56292c395f9f17405a8
h662 <h662hong@gmail.com>
2023-11-12 12:02:32 +0900

    Layout Setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  return (
+    <div className="bg-red-100 min-h-screen flex">
+      <div className="bg-blue-100 w-1/4 min-w-[120px]">Side</div>
+      <div className="bg-green-100 w-full flex flex-col">
+        <div className="bg-yellow-100 h-full">Main</div>
+        <div className="bg-purple-100 h-24 md:h-32">Chat</div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
