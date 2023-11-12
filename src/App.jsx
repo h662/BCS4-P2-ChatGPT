10668ea9ebf6dfd62a9cf4f45de4f773185061f0
h662 <h662hong@gmail.com>
2023-11-12 12:38:22 +0900

    ChatView Comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ChatSubmit from "./components/ChatSubmit";
+import ChatView from "./components/ChatView";
 
 const App = () => {
   const [content, setContent] = useState("");
@@ -8,7 +9,7 @@ const App = () => {
     <div className="min-h-screen flex">
       <div className="bg-blue-100 w-1/4 min-w-[120px]">Side</div>
       <div className="w-full flex flex-col">
-        <div className="bg-yellow-100 h-full">Main</div>
+        <ChatView />
         <ChatSubmit content={content} setContent={setContent} />
       </div>
     </div>
---
+++ src/components/ChatView.jsx
@@ -0,0 +1,98 @@
+const ChatView = () => {
+  return (
+    <div className="h-full py-4">
+      <ul className="text-xs h-full flex flex-col justify-end overflow-y-scroll">
+        <li className="flex flex-col">
+          <div className="bg-gray-50 ml-2 p-2 rounded-md w-fit">
+            Hello, I'm Chat GPT
+          </div>
+          <div className="bg-yellow-200 self-end  mr-2 p-2 rounded-md">
+            Hello?
+          </div>
+        </li>
+        <li className="flex flex-col">
+          <div className="bg-gray-50 ml-2 p-2 rounded-md w-fit">
+            Hello, I'm Chat GPT
+          </div>
+          <div className="bg-yellow-200 self-end  mr-2 p-2 rounded-md">
+            Hello?
+          </div>
+        </li>
+        <li className="flex flex-col">
+          <div className="bg-gray-50 ml-2 p-2 rounded-md w-fit">
+            Hello, I'm Chat GPT
+          </div>
+          <div className="bg-yellow-200 self-end  mr-2 p-2 rounded-md">
+            Hello?
+          </div>
+        </li>
+        <li className="flex flex-col">
+          <div className="bg-gray-50 ml-2 p-2 rounded-md w-fit">
+            Hello, I'm Chat GPT
+          </div>
+          <div className="bg-yellow-200 self-end  mr-2 p-2 rounded-md">
+            Hello?
+          </div>
+        </li>
+        <li className="flex flex-col">
+          <div className="bg-gray-50 ml-2 p-2 rounded-md w-fit">
+            Hello, I'm Chat GPT
+          </div>
+          <div className="bg-yellow-200 self-end  mr-2 p-2 rounded-md">
+            Hello?
+          </div>
+        </li>
+        <li className="flex flex-col">
+          <div className="bg-gray-50 ml-2 p-2 rounded-md w-fit">
+            Hello, I'm Chat GPT
+          </div>
+          <div className="bg-yellow-200 self-end  mr-2 p-2 rounded-md">
+            Hello?
+          </div>
+        </li>
+        <li className="flex flex-col">
+          <div className="bg-gray-50 ml-2 p-2 rounded-md w-fit">
+            Hello, I'm Chat GPT
+          </div>
+          <div className="bg-yellow-200 self-end  mr-2 p-2 rounded-md">
+            Hello?
+          </div>
+        </li>
+        <li className="flex flex-col">
+          <div className="bg-gray-50 ml-2 p-2 rounded-md w-fit">
+            Hello, I'm Chat GPT
+          </div>
+          <div className="bg-yellow-200 self-end  mr-2 p-2 rounded-md">
+            Hello?
+          </div>
+        </li>
+        <li className="flex flex-col">
+          <div className="bg-gray-50 ml-2 p-2 rounded-md w-fit">
+            Hello, I'm Chat GPT
+          </div>
+          <div className="bg-yellow-200 self-end  mr-2 p-2 rounded-md">
+            Hello?
+          </div>
+        </li>
+        <li className="flex flex-col">
+          <div className="bg-gray-50 ml-2 p-2 rounded-md w-fit">
+            Hello, I'm Chat GPT
+          </div>
+          <div className="bg-yellow-200 self-end  mr-2 p-2 rounded-md">
+            Hello?
+          </div>
+        </li>
+        <li className="flex flex-col">
+          <div className="bg-gray-50 ml-2 p-2 rounded-md w-fit">
+            Hello, I'm Chat GPT
+          </div>
+          <div className="bg-yellow-200 self-end  mr-2 p-2 rounded-md">
+            Hello?
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ChatView;
